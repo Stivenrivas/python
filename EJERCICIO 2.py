02279465a6b7592268b6408b554aadf613b32da5
Stivenrivas <danielmr28102006@gmail.com>
2025-07-22 16:20:23 -0500

    Update EJERCICIO 2.py

--- EJERCICIO 2.py
+++ EJERCICIO 2.py
@@ -1,10 +1,16 @@
 #hacer un programa que muestre los numeros enre dos numeros
 
-numero1 = int(input(" ingrese un numero"))
-
-numero2 = int(input(" ingrese un numero"))
+#aqui le pedimos los numeros al usuarios ingresdo dos numeros y nos aseguramos que solo se puede ingresar numeros
+while True:
+    try:
+        numero1 = int(input(" ingrese el primer numero"))
+        numero2 = int(input(" ingrese el segundo numero"))
+        break
+    except:
+        print("solo puede escribir numero")
 i = 0
 
+#aqui comparamos los dos numero para que la lista empice siempre con el mernor  y mostramos la lista
 if numero1 < numero2:
     for i in range(numero1+1, numero2):
         print(i)
